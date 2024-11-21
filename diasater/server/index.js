d83531285a6653179157b040a16593334088b777
prasadposimsetti <150354730+prasadposimsetti@users.noreply.github.com>
2024-11-22 00:03:47 +0530

    Add files via upload

---
+++ diasater/server/index.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+// server/index.js
+
+
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+
+
+app.use('/api/auth', authRoutes);
+const alertsRoute = require('./routes/alerts');
+app.use('/api/alerts', alertsRoute);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
